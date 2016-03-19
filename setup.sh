5d83176cd47aaf0cc8330d6feb59bea1ed2edaa3
Kenta Yamada <knt.yamada.800@gamil.com>
2016-03-19 15:57:41 +0900

    update setup program.

--- setup.sh
+++ setup.sh
@@ -12,20 +12,30 @@
 #gitリポジトリは全て「~/git/」にて管理
 if [ ! -e ~/git/ ]; then
     mkdir ~/git/
+    cd ~/git
+    echo "create directory."
 fi
 
-cd ~/git
 
 # dotfiles ダウンロード
-git clone 'https://github.com/KentaYamada/dotfiles.git'
+git clone "https://github.com/KentaYamada/dotfiles.git"
 
 if [ $? == 0 ]; then
-    mkdir ./bundle
+    cd ~/git/dotfiles/.vim
+    if [ ! -e ./bundle ]; then
+        mkdir ./bundle
+    fi
+    cd ./bundle
 else
     echo "git clone failed."
     exit 1
 fi
 
-#ToDo:配列とループを使って実行できるようにする
+# NeoBundleダウンロード
+git clone "https://github.com/Shougo/neobundle.vim.git"
+git submodule update --init
+
+
+# ToDo:配列とループを使って実行できるようにする
 ln -sf ~/git/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/git/dotfiles/.vim ~/.vim
