31ec548709c4ecdfa347770da4b8de9632c0c1f5
Kenta Yamada <knt.yamada.800@gmail.com>
2023-08-19 13:23:10 +0900

    補完周りの設定

--- nvim/lua/plugin-configs/lps.lua
+++ nvim/lua/plugin-configs/lps.lua
@@ -5,6 +5,7 @@
 -- https://github.com/williamboman/mason-lspconfig.nvim
 local mason = require('mason')
 local mason_lspconfig = require('mason-lspconfig')
+local lspconfig = require('lspconfig')
 
 mason.setup()
 mason_lspconfig.setup({
@@ -30,6 +31,48 @@ mason_lspconfig.setup({
     automatic_installation = false,
 })
 
+-- Complement configuration
+-- See: 
+local cmp = require('cmp')
+cmp.setup({
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        end,
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ['<Tab>'] = cmp.mapping.select_next_item(),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = {
+        {name = 'nvim_lsp'},
+        {name = 'buffer'},
+        {name = 'path'},
+        {name = 'vsnip'},
+    }
+})
+
+-- 補完時にLSPが効くようにする
+mason_lspconfig.setup_handlers({ function(server_name)
+    local cmplsp = require('cmp_nvim_lsp')
+    local opts = {
+        capabilities = cmplsp.default_capabilities(
+            vim.lsp.protocol.make_client_capabilities()
+        )
+    }
+    lspconfig[server_name].setup(opts)
+end })
+
 -- Linter, Formatter configuration
 local null_ls = require('null-ls')
 -- null_ls.setup({
@@ -37,6 +80,3 @@ local null_ls = require('null-ls')
 -- 
 --     }
 -- })
-
--- Complement configuration
-local cmp = require('cmp')
