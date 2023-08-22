c10f8f7899bde174aa0b570dd7c4c4e3295a2c6b
Kenta Yamada <knt.yamada.800@gmail.com>
2023-08-22 11:21:29 +0900

    move init.vim to init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,31 @@
+-- init.lua
+-- load lua file: require('module_name')
+-- load vimscript file: vim.cmd('source ' .. {filepath})
+
+-- Basic configs
+vim.cmd('source ' .. '~/.config/nvim/configs/environment.vim')
+vim.cmd('source ' .. '~/.config/nvim/configs/options.vim')
+require('basic-configs/options')
+require('basic-configs/keymaps')
+require('basic-configs/commands')
+require('basic-configs/triggers')
+-- Package manager
+require('plugin-configs/jetpack')
+-- Fuzzy finder
+require('plugin-configs/telescope')
+-- File explore
+require('plugin-configs/nvim-tree-lua')
+-- Status line plugin
+require('plugin-configs/lualine')
+-- Language server plugin
+-- source ~/.config/nvim/plugins/coc.vim
+require('plugin-configs/lps')
+-- Formatter
+require('plugin-configs/formatter')
+-- Colorscheme
+require('basic-configs/color')
+-- Syntax color
+require('plugin-configs/treesitter')
+-- Git
+require('plugin-configs/gitsigns')
+require('plugin-configs/neogit')
