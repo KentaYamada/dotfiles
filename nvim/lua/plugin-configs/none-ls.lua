d08031ba170bea17f3cb496852d9e666613c9e22
山田健太 <knt.yamada.800@gmail.com>
2024-02-06 22:10:19 +0900

    Refactoring none-ls config

--- nvim/lua/plugin-configs/none-ls.lua
+++ nvim/lua/plugin-configs/none-ls.lua
@@ -1,52 +1,36 @@
 --
 -- none-ls (null-ls) configuration
 --
-local async_formatting = function(bufnr)
-    bufnr = bufnr or vim.api.nvim_get_current_buf()
-
-    vim.lsp.buf_request(
-        bufnr,
-        "textDocument/formatting",
-        vim.lsp.util.make_formatting_params({}),
-        function(err, res, ctx)
-            if err then
-                local err_msg = type(err) == "string" and err or err.message
-                -- you can modify the log message / level (or ignore it completely)
-                vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
-                return
-            end
-
-            -- don't apply results if buffer is unloaded or has been modified
-            if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
-                return
-            end
-
-            if res then
-                local client = vim.lsp.get_client_by_id(ctx.client_id)
-                vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
-                vim.api.nvim_buf_call(bufnr, function()
-                    vim.cmd("silent noautocmd update")
-                end)
-            end
-        end
-    )
-end
+local null_ls = require("null-ls")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local formatting = null_ls.builtins.formatting
+local sources = {
+    formatting.prettier.with({
+        filetypes = {
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+            "json",
+            "css",
+            "scss",
+        }
+    })
+}
 local lsp_formatting = function(bufnr)
     vim.lsp.buf.format({
+        -- Avoiding LSP formatting conflicts
+        -- see: https://github.com/nvimtools/none-ls.nvim/wiki/Avoiding-LSP-formatting-conflicts#neovim-08
         filter = function(client)
             -- apply whatever logic you want (in this example, we'll only use null-ls)
             return client.name == "null-ls"
         end,
         bufnr = bufnr,
+        async = false,
     })
 end
-local null_ls = require("null-ls")
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.prettier,
-    },
+    sources = sources,
     debug = false,
     diagnostics_format = "#{m} (#{s}: #{c})",
     on_attach = function(client, bufnr)
@@ -57,13 +41,6 @@ null_ls.setup({
                 buffer = bufnr,
                 callback = function()
                     lsp_formatting(bufnr)
-                end,
-            })
-            vim.api.nvim_create_autocmd("BufWritePost", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    async_formatting(bufnr)
                 end,
             })
         end
