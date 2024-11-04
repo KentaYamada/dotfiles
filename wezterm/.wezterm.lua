a498cbcc5eadaace124fcf34406adaec32294f21
山田健太 <knt.yamada.800@gmail.com>
2024-11-04 20:42:36 +0900

    Modify: wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,30 +4,58 @@
 -- see: https://wezfurlong.org/wezterm/config/files.html
 --
 -- *******************************************************
-
--- Pull in the wezterm API
 local wezterm = require('wezterm')
 
-local config = {}
+-- This will hold the configuration.
+local config = wezterm.config_builder()
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
+config.automatically_reload_config = true
 
 -- This is where you actually apply your config choices
+
+-- see: https://wezfurlong.org/wezterm/colorschemes/index.html
 config.color_scheme = 'nightfox'
 config.window_background_opacity = 0.8
+config.macos_window_background_blur = 20
 
--- https://wezfurlong.org/wezterm/config/fonts.html#troubleshooting-fonts
+-- see: https://wezfurlong.org/wezterm/config/fonts.html#troubleshooting-fonts
 -- see: wezterm ls-fonts --list-system
 config.font = wezterm.font ('Hack Nerd Font')
+config.font_size = 12.0
 
 config.use_ime = true
 
-
 config.adjust_window_size_when_changing_font_size = false
 
--- and finally, return the configuration to wezterm
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+config.show_new_tab_button_in_tab_bar = false
+
+config.window_frame = {
+  inactive_titlebar_bg = "none",
+  active_titlebar_bg = "none",
+}
+
+config.window_background_gradient = {
+   colors = { "#000000" },
+}
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+   local background = "#5c6d74"
+   local foreground = "#FFFFFF"
+
+   if tab.is_active then
+     background = "#ae8b2d"
+     foreground = "#FFFFFF"
+   end
+
+   local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+
+   return {
+     { Background = { Color = background } },
+     { Foreground = { Color = foreground } },
+     { Text = title },
+   }
+ end)
+
 return config
