c7881b1a021edcbb51c75ebe04fcc9063d285681
山田健太 <knt.yamada.800@gmail.com>
2025-02-06 09:45:07 +0900

    Refactor color scheme config

--- nvim/lua/basic-configs/color.lua
+++ nvim/lua/basic-configs/color.lua
@@ -1,2 +1,16 @@
-vim.opt.termguicolors = true
-vim.cmd 'colorscheme nightfox'
+--
+-- Color scheme configuration
+-- See
+--  https://github.com/EdenEast/nightfox.nvim
+--
+
+local nightfox = require('nightfox')
+
+nightfox.setup({
+    options = {
+        terminal_colors = true,
+    },
+})
+
+-- setup must be called before loading
+vim.cmd ('colorscheme nightfox')
