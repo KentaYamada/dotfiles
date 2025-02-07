cab2c7f13f98afdb21c0ec939dc249db10fe9e93
山田健太 <knt.yamada.800@gmail.com>
2025-02-07 09:51:23 +0900

    Add keymap description

--- nvim/lua/plugin-configs/telescope.lua
+++ nvim/lua/plugin-configs/telescope.lua
@@ -4,9 +4,9 @@
 local telescope = require('telescope.builtin')
 
 -- pickers: https://github.com/nvim-telescope/telescope.nvim#pickers
-vim.keymap.set('n', '<leader>o', telescope.find_files, { noremap = true, silent = true})
-vim.keymap.set('n', '<leader>g', telescope.live_grep, { noremap = true, silent = true})
-vim.keymap.set('n', '<leader>G', telescope.grep_string, { noremap = true, silent = true})
-vim.keymap.set('n', '<leader>b', telescope.buffers, { noremap = true, silent = true})
-vim.keymap.set('n', '<leader>h', telescope.oldfiles, { noremap = true, silent = true})
-vim.keymap.set('n', '<leader>d', telescope.diagnostics, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>o', telescope.find_files, { noremap = true, silent = true, desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>g', telescope.live_grep, { noremap = true, silent = true, desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>G', telescope.grep_string, { noremap = true, silent = true, desc = 'Telescope live string' })
+vim.keymap.set('n', '<leader>b', telescope.buffers, { noremap = true, silent = true, desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>h', telescope.oldfiles, { noremap = true, silent = true, desc = 'Telescope oldfiles' })
+vim.keymap.set('n', '<leader>d', telescope.diagnostics, { noremap = true, silent = true, desc = 'Telescope diagnostics' })
