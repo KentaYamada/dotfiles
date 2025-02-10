a3efcb376c548464c0673c8882733af01948a7d1
山田健太 <knt.yamada.800@gmail.com>
2025-02-10 22:40:59 +0900

    Modify nvim-tree config

--- nvim/lua/plugin-configs/nvim-tree-lua.lua
+++ nvim/lua/plugin-configs/nvim-tree-lua.lua
@@ -3,35 +3,52 @@
 -- :h nvim-tree
 
 -- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+vim.api.nvim_set_var('loaded_netrw', 1)
+vim.api.nvim_set_var('loaded_netrwPlugin', 1)
 
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
--- local function my_on_attach(bufnr)
---     local api = require 'nvim-tree.api'
--- 
---     local function opts(desc)
---         return { desc = "nvim-tree: "..desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
---     end
--- 
---     -- default mappings
---     api.config.mappings.default_on_attach(bufnr)
--- 
---     -- custom mappings
---     vim.keymap.set('n', api.tree.toggle_help, opts('Help'))
--- end
+-- on attach handler
+local function my_on_attach(bufnr)
+    local api = require('nvim-tree.api')
 
-require("nvim-tree").setup {
---     on_attach = my_on_attach,
+    local function opts(desc)
+      return {
+          desc = "nvim-tree: " .. desc,
+          buffer = bufnr,
+          noremap = true,
+          silent = true,
+          nowait = true
+      }
+    end
+
+    -- default mappings
+    api.config.mappings.default_on_attach(bufnr)
+
+    -- custom mappings
+    vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+end
+
+require("nvim-tree").setup({
+    on_attach = my_on_attach,
     filters = {
+        dotfiles = false,
         git_ignored = false,
         custom = {
             -- ignore .git, but not .github
             "\\.git\\(\\(hub\\)\\@!\\)",
             "^node_modules",
         }
-    }
-}
-vim.keymap.set('n', '<leader>e', vim.cmd.NvimTreeToggle)
+    },
+    renderer = {
+        group_empty = true,
+    },
+    sort = {
+        folders_first = true,
+        sorter = "case_sensitive",
+    },
+})
+
+-- mappings
+vim.keymap.set('n', '<leader>e', vim.cmd.NvimTreeToggle, { desc = 'Toggle file explore' })
