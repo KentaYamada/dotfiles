9756ae9da7dee1e13e77c2bb12ae38279ef805d6
山田健太 <knt.yamada.800@gmail.com>
2026-01-22 10:51:13 +0900

    modify ensure install server list

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,49 +7,49 @@
 local mason = require("mason")
 local mason_lspconfig = require("mason-lspconfig")
 local servers = {
-	"clangd",
-	"eslint",
-	"lua_ls",
-	"pylsp",
-	"rust_analyzer",
-	"stylua",
-	"ts_ls",
-	"vimls",
+  "clangd",
+  "eslint_d",
+  "lua_ls",
+  "pylsp",
+  "rust_analyzer",
+  "stylua",
+  "ts_ls",
+  "vimls",
 }
 mason.setup()
 mason_lspconfig.setup({
-	ensure_installed = servers,
+  ensure_installed = servers,
 })
 
 -- Lua (Neovim dev friendly)
 vim.lsp.config("lua_ls", {
-	filetypes = { "lua" },
-	settings = {
-		Lua = {
-			diagnostics = { globals = { "vim" } },
-			workspace = { checkThirdParty = false },
-			telementry = { enable = true },
-		},
-	},
+  filetypes = { "lua" },
+  settings = {
+    Lua = {
+      diagnostics = { globals = { "vim" } },
+      workspace = { checkThirdParty = false },
+      telementry = { enable = true },
+    },
+  },
 })
 
 -- Rust
 vim.lsp.config("rust_analyzer", {
-	filetypes = { "rust" },
-	settings = {
-		["rust_analyzer"] = {
-			check = { command = "clippy" },
-		},
-	},
+  filetypes = { "rust" },
+  settings = {
+    ["rust_analyzer"] = {
+      check = { command = "clippy" },
+    },
+  },
 })
 
 -- Diagnostic
 vim.diagnostic.config({
-	serverity_sort = true,
-	signs = true,
-	underline = true,
-	update_in_insert = false,
-	virtual_text = false,
+  serverity_sort = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  virtual_text = false,
 })
 
 -- LSP有効化
@@ -67,43 +67,43 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-	callback = function(ev)
-		-- Buffer local mappings.
-		-- See `:help vim.lsp.*` for documentation on any of the below functions
-		local opts = { buffer = ev.buf }
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = ev.buf, desc = "宣言にジャンプ" })
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = ev.buf, desc = "定義にジャンプ" })
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = ev.buf, desc = "ホバー表示" })
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-		vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
-		vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
-		vim.keymap.set("n", "<leader>wl", function()
-			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-		end, opts)
-		vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = ev.buf, desc = "リネーム" })
-		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-		vim.keymap.set("n", "<leader>f", function()
-			vim.lsp.buf.format({ async = true })
-		end, opts)
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(ev)
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = ev.buf, desc = "宣言にジャンプ" })
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = ev.buf, desc = "定義にジャンプ" })
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = ev.buf, desc = "ホバー表示" })
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set("n", "<leader>wl", function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = ev.buf, desc = "リネーム" })
+    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<leader>f", function()
+      vim.lsp.buf.format({ async = true })
+    end, opts)
 
-		-- Format on save
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = vim.api.nvim_create_augroup("FormatOnSave", {}),
-			buffer = ev.buf,
-			callback = function()
-				-- only if server supports formatting
-				local clients = vim.lsp.get_clients({ buffer = ev.buf })
-				for _, client in ipairs(clients) do
-					if client.supports_method("textDocument/formatting") then
-						vim.lsp.buf.format({ async = false })
-						return
-					end
-				end
-			end,
-		})
-	end,
+    -- Format on save
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = vim.api.nvim_create_augroup("FormatOnSave", {}),
+      buffer = ev.buf,
+      callback = function()
+        -- only if server supports formatting
+        local clients = vim.lsp.get_clients({ buffer = ev.buf })
+        for _, client in ipairs(clients) do
+          if client.supports_method("textDocument/formatting") then
+            vim.lsp.buf.format({ async = false })
+            return
+          end
+        end
+      end,
+    })
+  end,
 })
