5a03242e43ec19f2e2509ab284a78ddab86413dc
山田健太 <knt.yamada.800@gmail.com>
2026-04-18 00:46:46 +0900

    fix wezterm font size

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -23,12 +23,13 @@ config.macos_window_background_blur = 15
 -- see: wezterm ls-fonts --list-system
 -- config.font = wezterm.font ('Hack Nerd Font')
 config.font = wezterm.font_with_fallback({
-	{ family = "JetBrainsMono Nerd Font", weight = "Regular" },
-	"Source Han Sans JP", -- 日本語
-	"Noto Color Emoji", -- Linux
-	"Apple Color Emoji", -- macOS
+  { family = "JetBrainsMono Nerd Font", weight = "Regular" },
+  "Source Han Sans JP", -- 日本語
+  "Noto Color Emoji",  -- Linux
+  "Apple Color Emoji", -- macOS
 })
-config.font_size = 12.0
+config.font_size = 14.0
+config.line_height = 1.2
 
 config.use_ime = true
 
@@ -40,12 +41,12 @@ config.show_new_tab_button_in_tab_bar = false
 config.use_fancy_tab_bar = true
 
 config.window_frame = {
-	inactive_titlebar_bg = "none",
-	active_titlebar_bg = "none",
+  inactive_titlebar_bg = "none",
+  active_titlebar_bg = "none",
 }
 
 config.window_background_gradient = {
-	colors = { "#000000" },
+  colors = { "#000000" },
 }
 
 -- for linux
@@ -53,39 +54,39 @@ config.front_end = "OpenGL"
 config.enable_wayland = false
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local background = "#5c6d74"
-	local foreground = "#FFFFFF"
+  local background = "#5c6d74"
+  local foreground = "#FFFFFF"
 
-	if tab.is_active then
-		background = "#ae8b2d"
-		foreground = "#FFFFFF"
-	end
+  if tab.is_active then
+    background = "#ae8b2d"
+    foreground = "#FFFFFF"
+  end
 
-	local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+  local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
 
-	return {
-		{ Background = { Color = background } },
-		{ Foreground = { Color = foreground } },
-		{ Text = title },
-	}
+  return {
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = title },
+  }
 end)
 
 wezterm.on("update-right-status", function(window, pane)
-	local date = wezterm.strftime("%Y-%m-%d %H:%M:%S")
+  local date = wezterm.strftime("%Y-%m-%d %H:%M:%S")
 
-	-- Make it italic and underlined
-	window:set_right_status(wezterm.format({
-		{ Attribute = { Underline = "Single" } },
-		{ Attribute = { Italic = true } },
-		{ Text = date },
-	}))
+  -- Make it italic and underlined
+  window:set_right_status(wezterm.format({
+    { Attribute = { Underline = "Single" } },
+    { Attribute = { Italic = true } },
+    { Text = date },
+  }))
 end)
 
 local mux = wezterm.mux
 wezterm.on("gui-startup", function(cmd)
-	-- 起動時に最大サイズで表示
-	local _tab, _pane, window = mux.spawn_window(cmd or {})
-	window:gui_window():maximize()
+  -- 起動時に最大サイズで表示
+  local _tab, _pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
 end)
 
 return config
