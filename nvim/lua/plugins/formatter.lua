e7a986ebc71c6d77d0d498ef2a0a98a1a6addd55
yamaken <knt.yamada.800@gmail.com>
2026-07-06 10:17:05 +0900

    remove format plugin

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -3,21 +3,21 @@
 -- see: https://github.com/stevearc/conform.nvim
 --
 local config = {
-	formatters_by_ft = {
-		lua = { "stylua" },
-		rust = { "rustfmt" },
-		css = { "prettierd", "prettier", stop_after_first = true },
-		scss = { "prettierd", "prettier", stop_after_first = true },
-		json = { "prettierd", "prettier", stop_after_first = true },
-		javascript = { "prettierd", "prettier", stop_after_first = true },
-		typescript = { "prettierd", "prettier", stop_after_first = true },
-		javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-		typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-	},
-	format_on_save = {
-		lsp_format = "fallback",
-		timeout_ms = 2000, -- 2 seconds
-	},
+  formatters_by_ft = {
+    lua = { "stylua" },
+    rust = { "rustfmt" },
+    css = { "prettierd", stop_after_first = true },
+    scss = { "prettierd", stop_after_first = true },
+    json = { "prettierd", op_after_first = true },
+    javascript = { "prettierd", op_after_first = true },
+    typescript = { "prettierd", op_after_first = true },
+    javascriptreact = { "prettierd", op_after_first = true },
+    typescriptreact = { "prettierd", op_after_first = true },
+  },
+  format_on_save = {
+    lsp_format = "fallback",
+    timeout_ms = 2000, -- 2 seconds
+  },
 }
 local conform = require("conform")
 
